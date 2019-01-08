921d9021106cf16f715aeeb61b01c007ef50224e
webdac <webdac@gmail.com>
2019-01-08 08:07:11 +0100

    Fix new line issue in tokenizer.

--- tokenizestemmer.js
+++ tokenizestemmer.js
@@ -5,7 +5,9 @@ const {stopwords} = require('./stopwordsEn.json')
 const filterstopw = (arr, opts = {}) =>
   opts.stopwords ? arr.filter(t => !stopwords.includes(t)) : arr
 
-const tokenize = (str, opts) => filterstopw(str.split(' ')
+const tokenize = (str, opts) => filterstopw(str
+  .replace(/\n|\t/g, ' ')
+  .split(' ')
   .filter(t => t.match(/[0-9a-z]/i))
   .map(t => accents.remove(t.toLowerCase()))
   .map(t => t.replace(/[^0-9a-z]/ig, '')), opts)
