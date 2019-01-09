b683c2e345c99adb770f44b54f50edfc93533805
webdac <webdac@gmail.com>
2019-01-09 08:40:00 +0100

    Changing implementation of uniquePhrases, repeatedPhrases and repeatedWords.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const soa = require('sort-objects-array')
 const {
   getTokensMap,
+  checkPhraseUniqueWords,
+  checkPhraseRepeatedWords,
   checkPhraseUnique,
   checkPhraseRepeated,
   checkArray
@@ -11,49 +13,48 @@ const uniqueWords = (str, opts = {}) => Array.from(
   .filter(arr => arr[1] === 1)
   .map(x => x[0])
 
-const repeatedWords = (str, opts = {}) => soa(
+const repeatedWords = (str, opts = {}) => Array.from(
+  getTokensMap(str, opts))
+  .filter(arr => arr[1] > (opts.min || 1))
+  .map(x => x[0])
+
+const mostCommonWordsCount = (str, opts = {}) => soa(
   Array.from(
     getTokensMap(str, opts)
   )
   .filter(arr => arr[1] > (opts.min || 1))
-  .reduce((k, v) => { k[v[0]] = v[1]; return k }, {}), 'key')
+  .reduce((k, v) => { k[v[0]] = v[1]; return k }, {}), 'value', {order: 'desc'})
 
-const mostCommonWords = (str, opts = {}) => soa(
-  repeatedWords(str, opts), 'value', {order: 'desc'})
+const uniquePhrases = (arr, opts = {}) => {
+  const tokensMap = getTokensMap(checkArray(arr).map(
+    x => x.replace(/ /g, '_')).join(' '), opts)
+  const filtered = checkArray(arr).filter(p => checkPhraseUnique(p, tokensMap, opts))
+  return filtered
+}
 
-const uniquePhrases = (arr, opts = {}) => Array.from(
-    getTokensMap(checkArray(arr)
-    .map(x => x.replace(/ /g, '_')
-  )
-  .join(' '), opts))
-  .filter(arr => arr[1] === 1)
-  .map(x => x[0].replace(/_/g, ' '))
-
-const repeatedPhrases = (arr, opts = {}) => soa(
-  Array.from(
-    getTokensMap(checkArray(arr)
-    .map(x => x.replace(/ /g, '_')
-  )
-  .join(' '), opts))
-  .filter(arr => arr[1] > (opts.min || 1))
-  .reduce((k, v) => { k[String(v[0]).replace(/_/g, ' ')] = v[1]; return k }, {}), 'key')
+const repeatedPhrases = (arr, opts = {}) => {
+  const tokensMap = getTokensMap(checkArray(arr).map(
+    x => x.replace(/ /g, '_')).join(' '), opts)
+  const filtered = checkArray(arr).filter(p => checkPhraseRepeated(p, tokensMap, opts))
+  return filtered
+}
 
 const phrasesWithUniqueWords = (arr, opts = {}) => {
   const tokensMap = getTokensMap(checkArray(arr).join(' '), opts)
-  const filtered = checkArray(arr).filter(p => checkPhraseUnique(p, tokensMap, opts))
+  const filtered = checkArray(arr).filter(p => checkPhraseUniqueWords(p, tokensMap, opts))
   return filtered
 }
 
 const phrasesWithRepeatedWords = (arr, opts = {}) => {
   const tokensMap = getTokensMap(checkArray(arr).join(' '), opts)
-  const filtered = checkArray(arr).filter(p => checkPhraseRepeated(p, tokensMap, opts))
+  const filtered = checkArray(arr).filter(p => checkPhraseRepeatedWords(p, tokensMap, opts))
   return filtered
 }
 
 module.exports = {
   uniqueWords,
   repeatedWords,
-  mostCommonWords,
+  mostCommonWordsCount,
   uniquePhrases,
   repeatedPhrases,
   phrasesWithUniqueWords,
